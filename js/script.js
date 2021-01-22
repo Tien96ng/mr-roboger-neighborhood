91d91caeee42cf76af98ebd45e5ef4758d653f03
tien96ng <tien96ng@ghmail.com>
2021-01-22 12:43:21 -0800

    Create array for input to string replacement.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const replyArr = ["Beep!", "Boop!", "Won't you be my neighbor?"];
+const tryAgainPrompt = "Enter in another number to get a new result!";
+
+// A user should be able to enter a new number and see new results over and over again.
+
+
 $(document).ready(function() {
+  
+  $("#form").submit(event => {
+    event.preventDefault();
+    let numInput = parseInt($("#input").val());
+    alert(typeof numInput);
+
+  });
 
 });
