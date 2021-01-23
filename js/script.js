f9af6818346bcb3b160da74de21c4d889cfa7e7a
tien96ng <tien96ng@ghmail.com>
2021-01-22 17:27:48 -0800

    Add functionality to replace 'neighbor' with user's name if they enter it in, which is optional

--- js/script.js
+++ js/script.js
@@ -1,23 +1,36 @@
-const replyArr = [`"Beep!"`, `"Boop!"`, `"Won't you be my neighbor?"`];
+let replyArr = [`"Beep!"`, `"Boop!"`, `"Won't you be my`];
 const tryAgainPrompt = "Enter in another number to get a new result!";
 let pTagStr = "<p>Here is your result: <p>";
+const neighbor = ` neighbor?"`;
+
 
 $(document).ready(function() {
+  let numInput = "";
+  let name = "";
   
   $("#form").submit(event => {
     event.preventDefault();
-    let numInput = parseInt($("#input").val());
+
+    $("#arr").empty();
+    $("#arr-reverse").empty();
+
+    numInput = parseInt($("#input").val());
+    name = `, ${$("#name").val()}?"`;
+
+    if(name === `, ?"`) {
+      name = neighbor;
+    }
     
-    const beepBoop = (range) => {
+    const beepBoop = (range, name) => {
       let output = [];
 
       for(let i = 0; i <= range; i++) {
         if(i.toString().includes("3")) {
-          output.push(replyArr[2])
+          output.push(replyArr[2] + name);
         } else if(i.toString().includes("2")) {
-          output.push(replyArr[1])
+          output.push(replyArr[1]);
         } else if(i.toString().includes("1")) {
-          output.push(replyArr[0])
+          output.push(replyArr[0]);
         } else {
           output.push(i)
         }
@@ -27,13 +40,15 @@ $(document).ready(function() {
 
     $("#try-again").text(tryAgainPrompt);
 
-    let reverseAnswer = beepBoop(numInput).reverse().join(", ");
-    let answer = beepBoop(numInput).join(", ");
+    let reverseAnswer = beepBoop(numInput, name).reverse().join(", ");
+    let answer = beepBoop(numInput, name).join(", ");
 
     $("#output-container").show();
-    $("#output").append(`<span id='arr'>${answer}</span>`)
-    $("#output-reverse").append(`<span id='arr-reverse'>${reverseAnswer}</span>`)
+    $("#output").append(`<span id='arr'>${answer}</span>`);
+    $("#output-reverse").append(`<span id='arr-reverse'>${reverseAnswer}</span>`);
 
     $("#form")[0].reset();
+    name = "";
+    
   });
 });
